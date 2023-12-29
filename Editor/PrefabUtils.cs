32eaefbd425a336dbfabf778b4308f1824870903
Aaron Grabowy <8436565+detzt@users.noreply.github.com>
2023-12-29 08:44:08 +0100

    Added PrefabUtils

---
+++ Editor/PrefabUtils.cs
@@ -0,0 +1,63 @@
+using UnityEditor;
+using UnityEngine;
+
+/// <summary>
+/// Collection of utility functions for working with prefabs
+/// </summary>
+public static class PrefabUtils {
+
+    /// <summary>
+    /// Reverts the local rotation and position if they are identical to its source prefab
+    /// </summary>
+    /// <param name="transform">The Transform to change</param>
+    public static void RevertIdentityTransforms(Transform transform) {
+        var reference = PrefabUtility.GetCorrespondingObjectFromSource(transform);
+        if(reference != null) RevertIdentityTransforms(transform, reference.transform);
+    }
+
+    /// <summary>
+    /// Reverts the local rotation and position if they are identical to the given <paramref name="reference"/> Transform
+    /// </summary>
+    /// <param name="transform">The Transform to change</param>
+    /// <param name="reference">Reference Transform</param>
+    public static void RevertIdentityTransforms(Transform transform, Transform reference) {
+        using var so = new SerializedObject(transform);
+        Undo.RecordObject(transform, "Revert Identity Transforms");
+        PrefabUtility.RecordPrefabInstancePropertyModifications(transform);
+
+        var posDiff = transform.position - reference.position;
+        if(transform.rotation == reference.rotation)
+            PrefabUtility.RevertPropertyOverride(so.FindProperty("m_LocalRotation"), InteractionMode.AutomatedAction);
+        if(Mathf.Abs(posDiff.x) < float.Epsilon)
+            PrefabUtility.RevertPropertyOverride(so.FindProperty("m_LocalPosition.x"), InteractionMode.AutomatedAction);
+        if(Mathf.Abs(posDiff.y) < float.Epsilon)
+            PrefabUtility.RevertPropertyOverride(so.FindProperty("m_LocalPosition.y"), InteractionMode.AutomatedAction);
+        if(Mathf.Abs(posDiff.z) < float.Epsilon)
+            PrefabUtility.RevertPropertyOverride(so.FindProperty("m_LocalPosition.z"), InteractionMode.AutomatedAction);
+
+        _ = so.ApplyModifiedProperties();
+    }
+
+    /// <summary>
+    /// Returns whether the given <paramref name="transform"/> has default overrides that are identical to its source prefab
+    /// </summary>
+    /// <param name="transform">The Transform to check</param>
+    public static bool HasIdenticalTransformOverrides(Transform transform) {
+        var reference = PrefabUtility.GetCorrespondingObjectFromSource(transform);
+        return reference != null && HasIdenticalTransformOverrides(transform, reference);
+    }
+
+    /// <summary>
+    /// Returns whether the given <paramref name="transform"/> has default overrides that are identical to the given <paramref name="reference"/>
+    /// </summary>
+    /// <param name="transform">The Transform to check</param>
+    /// <param name="reference">The reference Transform to compare to</param>
+    public static bool HasIdenticalTransformOverrides(Transform transform, Transform reference) {
+        using var so = new SerializedObject(transform);
+        var posDiff = transform.position - reference.position;
+        return transform.rotation == reference.rotation && so.FindProperty("m_LocalRotation").prefabOverride ||
+               Mathf.Abs(posDiff.x) < float.Epsilon && so.FindProperty("m_LocalPosition.x").prefabOverride ||
+               Mathf.Abs(posDiff.y) < float.Epsilon && so.FindProperty("m_LocalPosition.y").prefabOverride ||
+               Mathf.Abs(posDiff.z) < float.Epsilon && so.FindProperty("m_LocalPosition.z").prefabOverride;
+    }
+}
